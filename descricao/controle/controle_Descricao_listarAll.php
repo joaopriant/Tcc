29d7b24839e5203c58b38e79b88dace373157f17
João Priante <92949954+joaopriant@users.noreply.github.com>
2023-05-10 17:10:13 -0300

    Delete descricao directory

--- descricao/controle/controle_Descricao_listarAll.php
+++
@@ -1,8 +0,0 @@
-<?php
-    require_once "../Descricao/Descricao.php";
-    $Descricao = new Descricao();
-   
-    $objDescricao = $Descricao->listarDescricao();
-    echo json_encode($objDescricao);
-
-?>
