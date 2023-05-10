bb4f8f28bf757a4eb1776ddcfd868fa6e4363a48
João Priante <92949954+joaopriant@users.noreply.github.com>
2023-05-10 17:11:18 -0300

    Delete Equipamento directory

--- Equipamento/controle/controle_Equipamento_listarAll.php
+++
@@ -1,8 +0,0 @@
-<?php
-    require_once "../Equipamento/equipamento.php";
-   $equipamento = new Equipamento();
-   
-   $objetoequipamento = $equipamento->listarEquipamento();
-   echo json_encode($objetoequipamento);
-
-?>
