f0ff1a83c12ca3dc8032e3b4377b0706268e6576
João Priante <92949954+joaopriant@users.noreply.github.com>
2023-05-10 17:09:33 -0300

    Delete manutencao directory

--- manutencao/controle/controle_manutencao_listarAll.php
+++
@@ -1,8 +0,0 @@
-<?php
-    require_once "../manutencao/manutencao.php";
-   $manutencao = new manutencao();
-   
-   $objetomanutencao = $manutencao->listarmanutencao();
-   echo json_encode($objetomanutencao);
-
-?>
