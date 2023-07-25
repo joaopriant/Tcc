c82e9b69e902acf4efa9b0fd6e5995b5f24369ee
João Priante <jm.priante@gmail.com>
2023-07-25 12:41:08 -0300

    equip

--- controle/equipamento/controle_Equipamento_cadastrar.php
+++ controle/equipamento/controle_Equipamento_cadastrar.php
@@ -1,36 +1,62 @@
 <?php
-require_once "../equipamento/equipamento.php";
+require_once "../../modelo/Equipamento.php";
+
+$request_raw = file_get_contents('php://input');
+$json_object = json_decode($request_raw);
+
+if($json_object!=null){
+    
+    $idequipamento = $json_object->idequipamento;
+    $idequipamento  = strip_tags($idequipamento);
+
+    $numpatrimonio = $json_object->numpatrimonio;
+    $numpatrimonio  = strip_tags($numpatrimonio);
+    
+    $local = $json_object->local;
+    $local  = strip_tags($local);
+    
+    $responsavel = $json_object->responsavel;
+    $responsavel  = strip_tags($responsavel);
+
+    $descricao = $json_object->descricao;
+    $descricao  = strip_tags($descricao);
+
+    if ($idequipamento=="") {
+        echo '{"cod":"1","msg":"O id não pode ser vazio!"}';
+        exit;
+    }
+    if ($descricao=="") {
+        echo '{"cod":"2","msg":"A descricao não pode ser vazio!"}';
+        exit;
+    }
+
+    if ($responsavel=="") {
+        echo '{"cod":"3","msg":"O id não pode ser vazio!"}';
+        exit;
+    }
+    if ($local=="") {
+        echo '{"cod":"5","msg":"local não pode ser vazio!"}';
+        exit;
+    }
 
 
-if (!isset($_POST['txtnumPatrimonio'])) {
-    die("Num patrimonio não encontrado\n");
-}
+    $Equipamento = new Equipamento();
+    $Equipamento->setIdEquipamento($idequipamento);
+    $Equipamento->setRegistroFuncionario($descricao);
+    $Equipamento->setIdsala($local);
+    $Equipamento->setRegistroFuncionario($responsavel);
+    $Equipamento->setnumpatrimonio($numpatrimonio);
+  
+    $resultado = $Equipamento->cadastrar();
 
-
-
-$numpatri = $_POST['txtnumPatrimonio'];
-$locais = $_POST['cbolocais'];
-$funcionarios = $_POST['cbofuncionario'];
-$descricao = $_POST['cbodescricao'];
-
-$numpatri = strip_tags($numpatri);
-$locais = strip_tags($locais);
-$funcionarios = strip_tags($funcionarios);
-$descricao = strip_tags($descricao);
-
-$Equipamento = new Equipamento();
-$Equipamento->setnumPatrimonio($numpatri);
-$Equipamento->setidDescricao($descricao);
-$Equipamento->setIdsala($locais);
-$Equipamento->setRegistroFuncionario($funcionarios);
-
-
-
-$resultado = $Equipamento->cadastrar();
-if ($resultado == true) {
-    echo "Cadastrado com sucesso";
-} else {
-    echo "Erro ao cadastrar";
+    if ($resultado == true) {
+        echo '{"cod":"7","msg":"Cadastrado com seucesso"}';
+    } else {
+        echo '{"cod":"8","msg":"O cargo não pode ser vazio!"}';
+    }
+}else{
+    echo '{"cod":"9","msg":"O cargo não pode ser nulo!"}';
+    exit;
 }
 
 ?>
